431592b87fe87a7822e6bef07124967a2065937e
AnotherLie <lordring66@gmail.com>
2019-03-12 18:05:03 +0100

    Added DropDownMenu

---
+++ LearningASPCORE/Controllers/DiaryController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LearningASPCORE.Models;
+using System.Diagnostics;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LearningASPCORE.Controllers
+{
+    public class DiaryController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Diary()
+        {
+            ViewData["Message"] = "My Diary";
+            return View();
+        }
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
